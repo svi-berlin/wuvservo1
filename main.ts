d27290f56dd2d948a506dee985a34e0d80e8f990
svi-berlin <svi-berlin@users.noreply.github.com>
2021-11-15 14:41:21 +0100

    Fred spricht beim Start und wechselt danach die Farben der LED wenn eine Taste gedrückt wird.

--- main.ts
+++ main.ts
@@ -3,20 +3,14 @@ input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P1, 0)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.setLedColor(0x00ff00)
-    pins.servoWritePin(AnalogPin.P1, 90)
+    basic.setLedColor(0xff0000)
 })
 input.onButtonPressed(Button.B, function () {
     basic.setLedColor(0x0000ff)
-    pins.servoWritePin(AnalogPin.P1, 180)
 })
 serialmp3.connectSerialMp3(DigitalPin.C16, DigitalPin.C17)
-basic.setLedColor(0x00ff00)
 serialmp3.playMp3Track(1, 1)
-basic.pause(1000)
-basic.setLedColor(0xffffff)
-pins.servoWritePin(AnalogPin.P1, 90)
-basic.setLedColor(0xff0000)
+basic.setLedColor(0x00ff00)
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
     basic.pause(200)
